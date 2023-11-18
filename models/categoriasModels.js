e57d49fb5b69edd6c9a5c9ec176c0f137dc1da4c
Aaron Rosales <rosalesramirezaa@uvm.edu.ve>
2023-11-18 17:26:42 -0400

    Commit 14 - Correccion CRUD - Categoria terminado

--- models/categoriasModels.js
+++ models/categoriasModels.js
@@ -1,56 +1,77 @@
 const { v4: uuidv4 } = require('uuid');
-
-let categoriaDB = [
-{
-    nombre: "Sumo",
-    id: "302",
-},
-{   
-    nombre: "Incapacidad",
-    id: "221",
-
-},
-{
-    nombre: "Seguidor de línea",
-    id: "201",
-},
-{
-    nombre: "Recolección de objetos",
-    id: "209",
-},
-{
-    nombre: "Estudia el el problema y presenta un prototipo que represente el problema y la solución",
-    id: "78",
-}
-];
+const database = require("../db/db");
 
 class categoriaModels{
     //Buscar todos
-    todos(){
-       return categoriaDB;
+    async todos(){
+        try{
+            const connection = await database.getConnection();
+            const result = await connection.query("SELECT categorias.id_cat, categorias.nombre_cat, modalidades.nombre_mod FROM categorias, modalidades WHERE categorias.id_mod = modalidades.id_mod");
+            return result;
+        } catch(error){
+            return ("Error en la consulta.....");
+        }
     }
     //Buscar por ID
-    buscarID(id){
-       const categoriaid = categoriaDB.find (categoria => categoria.id === id);
-       return categoriaid
+     async buscarID(id){
+       try {
+            const connection = await database.getConnection();
+            const result = await connection.query("SELECT categorias.id_cat, categorias.nombre_cat, modalidades.nombre_mod FROM categorias, modalidades WHERE categorias.id_mod = modalidades.id_mod AND categorias.id_cat=?",id);
+            if (result.length==0){
+                return ("Categoría no Encontrada....");
+            }
+            return result;
+
+        } catch (error) {
+            return ("Error en la consulta.....");
+        }
     }
     //Crear
-    crear(categoria){
-        categoria.id = uuidv4();
-        categoriaDB.push(categoria);
+    async crear(nuevacat){
+        try {
+            //const id = uuidv4();
+            //const id=usuario.id;
+            const nombre_cat = nuevacat.nombre_cat;
+            const id_mod=nuevacat.id_mod;
+            const datos ={nombre_cat,id_mod};
+            if (nombre_cat === undefined ) {
+                return ("Nombre requerido..." );
+            }
+            const connection = await database.getConnection();
+            await connection.query("INSERT INTO categorias SET ?", datos);
+            return ("Usuario ingresado con exito...");
+        } catch (error) {
+            return ("Error en la consulta.....");
+        }
     }
     //Modificar
-    modificar(id, nuevaCategoria){
-        const categoria = categoriaDB.find (categoria => categoria.id === id);
-        if (categoria) {
-        categoria.nombre = nuevaCategoria;
-        return categoriaDB;
+    async modificar(id, nuevacat){
+        try {
+            if (id === undefined || nuevacat.nombre_cat === undefined || nuevacat.id_mod===undefined) {
+                return ("Datos Incompletos.." );
+            }
+            const datos = {nuevacat};
+            const connection = await database.getConnection();
+            const result = await connection.query("UPDATE categorias SET ? WHERE id_cat=?", [nuevacat,id]);
+            return result;
+        } catch (error) {
+            return ("Error en la consulta.....");
+        }
     }
-    }
+
     //Eliminar
-    eliminar(id){
-        const categoriaEliminada = categoriaDB.filter (categoria => categoria.id !== id);
-        return categoriaEliminada;
+    async eliminar(id){
+        try {
+            const connection = await database.getConnection();
+            const result = await connection.query("DELETE FROM categorias WHERE id_cat = ?", id);
+            if (result.affectedRows==0){
+                return ("Categoría no Encontrada....");
+            }
+            return result;
+        } catch (error) {
+            return ("Error en la consulta.....");
+        }
     }
 };
+
 module.exports = new categoriaModels();
