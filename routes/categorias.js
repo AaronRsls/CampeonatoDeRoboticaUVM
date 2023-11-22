f2ce3e912d3688c8104789d25872058ca0616af1
José Barrios <barriosbricenojoad@uvm.edu.ve>
2023-11-22 19:48:23 -0400

    Commit 29: Agregados los permisos para las rutas de categorías

--- routes/categorias.js
+++ routes/categorias.js
@@ -1,28 +1,29 @@
 var express = require('express');
 var router = express.Router();
-var categoriasController = require('../controlers/categorias-c')
+var categoriasController = require('../controlers/categorias-c');
+var verificarPermisos = require('../middleware/verificarpermisos');
 
 /* GET Categorias */
-router.get('/', async function(req, res, next) {
+router.get('/', verificarPermisos(['Admin','Editor']), async function(req, res, next) {
   res.send(await categoriasController.todos());
 });
 
 /* GET categorias por id */
-router.get('/:id', async function(req, res, next) {
+router.get('/:id', verificarPermisos(['Admin','Editor']), async function(req, res, next) {
   let id= req.params.id;
   res.send(await categoriasController.uno(id));
 });
 
 /* POST categorias */
-router.post('/', async function(req, res, next) {
+router.post('/', verificarPermisos(['Admin']), async function(req, res, next) {
     res.send(await categoriasController.crear(req.body));
 });
 /* PUT categorias */
-router.put('/:id', async function(req, res, next) {
+router.put('/:id', verificarPermisos(['Admin']), async function(req, res, next) {
   res.send(await categoriasController.modificar(req.params.id, req.body));
 });
 /* DELETE categorias */
-router.delete('/:id', async function(req, res, next) {
+router.delete('/:id', verificarPermisos(['Admin']), async function(req, res, next) {
   res.send(await categoriasController.eliminar(req.params.id));
 });
 
