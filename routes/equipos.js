747f18e24d5b612d33a0bfac29e286b266aef616
José Barrios <barriosbricenojoad@uvm.edu.ve>
2023-11-22 19:36:25 -0400

    Commit 28: Agregados los permisos en la ruta Equipos

--- routes/equipos.js
+++ routes/equipos.js
@@ -1,30 +1,31 @@
 var express = require('express');
 var router = express.Router();
 var equiposController = require('../controlers/equipos-c')
+var verificarPermisos = require('../middleware/verificarpermisos');
 
 /* GET equipos */
-router.get('/', async function(req, res, next) {
+router.get('/', verificarPermisos(['Admin','Editor']), async function(req, res, next) {
   res.send(await equiposController.todos());
 });
 
 /* GET equipos por id */
-router.get('/:id', async function(req, res, next) {
+router.get('/:id', verificarPermisos(['Admin','Editor']), async function(req, res, next) {
   let id= req.params.id;
   res.send(await equiposController.uno(id));
 });
 
 /* POST equipos */
-router.post('/', async function(req, res, next) {
+router.post('/', verificarPermisos(['Admin']), async function(req, res, next) {
     res.send(await equiposController.crear(req.body));
 });
 
 /* PUT equipos */
-router.put('/:id', async function(req, res, next) {
+router.put('/:id',  verificarPermisos(['Admin']), async function(req, res, next) {
   res.send(await equiposController.modificar(req.params.id, req.body.nombre_equipo));
 });
 
 /* DELETE equipos */
-router.delete('/:id', async function(req, res, next) {
+router.delete('/:id',  verificarPermisos(['Admin']), async function(req, res, next) {
   res.send(await equiposController.eliminar(req.params.id));
 });
 
