dfb23c0151972d5a650b00ddb3c3a3d8706aeffd
Aaron Rosales <rosalesramirezaa@uvm.edu.ve>
2023-11-22 17:39:48 -0400

    Commit 25: Permisos agregados en la ruta de Usuarios

--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,31 @@
 var express = require('express');
 var router = express.Router();
 var usuariosController = require('../controlers/users-c')
+var verificarPermisos = require('../middleware/verificarpermisos');
 
 /* GET users */
-router.get('/', async function(req, res, next) {
+router.get('/', verificarPermisos(['Admin','Editor']), async function(req, res, next) {
   res.send(await usuariosController.todos());
 });
 
 /* GET users por id */
-router.get('/:id', async function(req, res, next) {
+router.get('/:id', verificarPermisos(['Admin','Editor']), async function(req, res, next) {
   let id= req.params.id;
   res.send(await usuariosController.uno(id));
 });
 
 /* POST users */
-router.post('/', async function(req, res, next) {
+router.post('/', verificarPermisos(['Admin']), async function(req, res, next) {
   res.send(await usuariosController.crear(req.body))
 });
 
 /* PUT users */
-router.put('/:id', async function(req, res, next) {
+router.put('/:id', verificarPermisos(['Admin']), async function(req, res, next) {
   res.send(await usuariosController.modificar(req.params.id, req.body.nombre));
 });
 
 /* DELETE users */
-router.delete('/:id', async function(req, res, next) {
+router.delete('/:id', verificarPermisos(['Admin']),async function(req, res, next) {
   res.send(await usuariosController.eliminar(req.params.id));
 });
 
