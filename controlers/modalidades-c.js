51b1a24d57674794c2cad4165db6f3d1955b931e
Alexander Méndez <mendezgonzalezar@uvm.edu.ve>
2023-11-29 16:08:54 -0400

    Commit 34: CRUD de modalidades Completado

--- controlers/modalidades-c.js
+++ controlers/modalidades-c.js
@@ -1,5 +1,8 @@
 const database = require("../db/db");
 
+//*********************************************************** */
+// Clase para manejar CRUD de Modalidades
+//********************************************************** */
 class modalidadesController {
    
     // Metodo encargado de listar Modalidades
@@ -12,7 +15,67 @@ class modalidadesController {
             res.redirect('/');
         }
     }
+    
+    // Metodo encargado de buscar Modalidad por id
+    async uno(req,res) {
+        try {
+            const connection = await database.getConnection();
+            const result = await connection.query("SELECT * FROM modalidades WHERE id_mod = ?",req.params.id);
+            if (result.length==0){
+                res.send("Modalidad no Existe....");
+            }
+            res.render('editarmodalidad',{modalidad:result[0]});;
+        } catch (error) {
+            res.send("Error en la consulta.....");
+        }    
+    }
+    
+    // Metodo encargado de renderizar la vista para el formulario de ingreso de Modelidades
+    async crear(req,res) {
+        res.render('crearmodalidad');
+    }
 
+    // Metodo para insertar un nuevo registro a Modalidades
+    async nueva(req,res) {
+            try {
+                const connection = await database.getConnection();
+                await connection.query("INSERT INTO modalidades SET ?", req.body);
+                res.redirect('/modalidad');
+            } catch (error) {
+                res.send("Error en la consulta.....");
+            }
+    }
+
+    // Metodo para actualizar una Modalidad seleccionada
+    async modificar(req,res) {
+        try {            
+            const id=req.params.id;
+            const modalidad=req.body.nombre_mod;
+            if (id === undefined || modalidad === undefined) {
+                res.send("Datos Incompletos.." );
+            }
+            const datos = {modalidad};
+            const connection = await database.getConnection();
+            const result = await connection.query("UPDATE modalidades SET nombre_mod= ? WHERE id_mod = ?", [modalidad,id]);
+            res.redirect('/modalidad');
+        } catch (error) {
+            res.send("Error en la consulta.....");
+        }
+    }
+
+    // Metodo para eliminar una Modalidad
+    async eliminar(req,res) {
+        try {
+            const connection = await database.getConnection();
+            const result = await connection.query("DELETE FROM modalidades WHERE id_mod = ?", req.params.id);
+            if (result.affectedRows==0){
+                res.send("Modalidad no Encontrado....");
+            }
+            res.redirect('/modalidad');
+        } catch (error) {
+            res.send("Error en la consulta.....");
+        }
+    }
 };
 
 module.exports = new modalidadesController();
--- routes/modalidad.js
+++ routes/modalidad.js
@@ -5,5 +5,10 @@ var verificarPermisos = require('../middleware/verificarpermisos');
 
 /* GET modalidades */
 router.get('/', verificarPermisos(['Admin','Editor']), modalidadesController.todos);
+router.get('/editar/:id', verificarPermisos(['Admin','Editor']), modalidadesController.uno);
+router.get('/crear/', verificarPermisos(['Admin','Editor']), modalidadesController.crear);
+router.post('/nueva/', verificarPermisos(['Admin']), modalidadesController.nueva);
+router.post('/modificar/:id', verificarPermisos(['Admin','Editor']), modalidadesController.modificar);
+router.get('/eliminar/:id', verificarPermisos(['Admin']), modalidadesController.eliminar);
 
 module.exports = router;
