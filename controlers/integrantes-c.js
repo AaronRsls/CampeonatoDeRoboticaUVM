172893f9920d97db91c58bdf346161aa5723a731
Aaron Rosales <rosalesramirezaa@uvm.edu.ve>
2023-11-29 21:29:10 -0400

    Commit 39: Agregar CRUD de Integrante

--- controlers/integrantes-c.js
+++ controlers/integrantes-c.js
@@ -1,20 +1,82 @@
-var integrantesModels = require('../models/integrantesModels'); 
+const database = require("../db/db");
 
+//*********************************************************** */
+// Clase para manejar CRUD de Integrantes de los Equipos
+//********************************************************** */
 class integrantesController {
-    async todos() {
-        return await integrantesModels.todos();
+   
+    // Metodo encargado de listar Integrantes
+    async todos (req,res) {
+        try{
+            const connection = await database.getConnection();
+            const arrayintegrantes = await connection.query("SELECT integrantes.id_integrante, integrantes.nombre_integrante, equipos.nombre_equipo FROM integrantes, equipos WHERE integrantes.id_equipo = equipos.id_equipo");
+            res.render('integrantes',{arrayintegrantes});
+        } catch(error){
+            res.redirect('/');
+        }
     }
-    async uno(id) {
-        return await integrantesModels.buscarID(id);
+    
+    // Metodo encargado de buscar Integrante por id
+    async uno(req,res) {
+        try {
+            const connection = await database.getConnection();
+            const result = await connection.query("SELECT integrantes.id_integrante, integrantes.nombre_integrante, equipos.nombre_equipo FROM integrantes, equipos WHERE integrantes.id_equipo = equipos.id_equipo AND integrantes.id_integrante=?",req.params.id);
+            if (result.length==0){
+                res.send("Categoría no Existe....");
+            }
+            const connection2 = await database.getConnection();
+            const listaequipos = await connection2.query("SELECT * from equipos");
+            res.render('editarintegrante',{integrante:result[0],listaequipos});;
+        } catch (error) {
+            res.send("Error en la consulta.....");
+        }    
     }
-    async crear(integrante) {
-       return await integrantesModels.crear(integrante);
+
+    // Metodo encargado de Generar un listado de equipos para el select
+    async crear(req,res) {
+        const connection = await database.getConnection();
+        const listaequip = await connection.query("SELECT * from equipos");
+        res.render('crearintegrante',{listaequip});
     }
-    async modificar(id, nuevoIntegrante) {
-        return await integrantesModels.modificar(id, nuevoIntegrante);
+
+    // Metodo para insertar un nuevo integrante
+    async nuevo(req,res) {
+            try {
+                const connection = await database.getConnection();
+                await connection.query("INSERT INTO integrantes SET ?", req.body);
+                res.redirect('/integrantes');
+            } catch (error) {
+                res.send("Error en la consulta.....");
+            }
     }
-    async eliminar(id) {
-        return await integrantesModels.eliminar(id);
+    
+    // Metodo para modificar un integrante seleccionado
+    async modificar(req,res) {
+        try {            
+            const id=req.params.id;
+            const nombre_integrante=req.body.nombre_integrante;
+            const id_equipo=req.body.id_equipo;
+            const datos = {nombre_integrante,id_equipo};
+            const connection = await database.getConnection();
+            const result = await connection.query("UPDATE integrantes SET ? WHERE id_integrante = ?", [datos,id]);
+            res.redirect('/integrantes');
+        } catch (error) {
+            res.send("Error en la consulta.....");
+        }
+    }
+    
+    // Metodo para eliminar un Integrante
+    async eliminar(req,res) {
+        try {
+            const connection = await database.getConnection();
+            const result = await connection.query("DELETE FROM integrantes WHERE id_integrante = ?", req.params.id);
+            if (result.affectedRows==0){
+                res.send("Categoría no Encontrada....");
+            }
+            res.redirect("/integrantes");
+        } catch (error) {
+            res.send("Error en la consulta.....");
+        }
     }
 };
 
--- routes/integrantes.js
+++ routes/integrantes.js
@@ -1,30 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var integrantesController = require('../controlers/integrantes-c')
+var integrantesController = require('../controlers/integrantes-c');
 var verificarPermisos = require('../middleware/verificarpermisos');
 
-/* GET integrantes */
-router.get('/', verificarPermisos(['Admin','Editor']), async function(req, res, next) {
-  res.send(await integrantesController.todos());
-});
-
-/* GET integrantes por id */
-router.get('/:id', verificarPermisos(['Admin','Editor']), async function(req, res, next) {
-  let id= req.params.id;
-  res.send(await integrantesController.uno(id));
-});
-
-/* POST integrantes */
-router.post('/', verificarPermisos(['Admin']), async function(req, res, next) {
-    res.send(await integrantesController.crear(req.body));
-});
-/* PUT integrantes */
-router.put('/:id', verificarPermisos(['Admin']), async function(req, res, next) {
-  res.send(await integrantesController.modificar(req.params.id, req.body));
-});
-/* DELETE integrantes */
-router.delete('/:id', verificarPermisos(['Admin']), async function(req, res, next) {
-  res.send(await integrantesController.eliminar(req.params.id));
-});
+router.get('/', verificarPermisos(['Admin','Editor']), integrantesController.todos);
+router.get('/editar/:id', verificarPermisos(['Admin','Editor']), integrantesController.uno);
+router.get('/crear/', verificarPermisos(['Admin','Editor']), integrantesController.crear);
+router.post('/nuevo/', verificarPermisos(['Admin','Editor']), integrantesController.nuevo);
+router.post('/modificar/:id', verificarPermisos(['Admin','Editor']), integrantesController.modificar);
+router.get('/eliminar/:id', verificarPermisos(['Admin','Editor']), integrantesController.eliminar);
 
 module.exports = router;
